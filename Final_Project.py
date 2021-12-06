6cd6ef71b3a4f5cf750e9497bef5941056beca29
jimmyk2 <91558577+jimmyk2@users.noreply.github.com>
2021-12-06 12:44:22 -0800

    Add files via upload

--- Final_Project.py
+++ Final_Project.py
@@ -22,11 +22,13 @@ import matplotlib.pyplot as plt
 import datetime
 from sklearn.linear_model import LinearRegression
 from sklearn import linear_model
+import plotly.express as px
 
 st.title("Math 10 Final Project")
 
 
-df = pd.read_csv("https://raw.githubusercontent.com/jimmyk2/Final_Project/main/draftkings_contest_history_(11-23-21).csv", na_values=" ", )
+
+df = pd.read_csv("/Users/jimmy/Downloads/draftkings_contest_history_(11-23-21).csv", na_values=" ", )
 df["Date"]= df["Contest_Date_EST"].map(lambda x:x[:-5])
 df["Date"]= pd.to_datetime(df["Date"])
 df['Year']=df['Date'].dt.year
@@ -45,7 +47,7 @@ df["Net"]= df["Winnings_Non_Ticket"]- df["Entry_Fee"]
 
 
 
-st.write("### I am basing my project on a dataset that has been tracking my bets on the Draftkings platform. I am an avid sports fan and like to have my skin in the games I watch and I have never been able to see what my performance has been like.",df)
+st.write("### I am basing my project on a dataset that has been tracking my bets on the Draftkings platform. I am an avid sports fan and like to have my skin in the games I watch and I have never been able to see what my performance has been like.")
 df= df[["Sport","Game_Type","Entry","Date","Place","Contest_Entries","Place%","Winnings_Non_Ticket","Entry_Fee","Net"]].copy()
 
 
@@ -62,19 +64,27 @@ st.write("Some notable facts that I extracted from this data shows that I am rel
          "This is all with me only entering contests of at most $",df["Entry_Fee"].max(),
          "while having my biggest winnings being of $",df["Winnings_Non_Ticket"].max(),
          "It is cool to see how much success I have with relatively low stakes and a win rate of",
-         (((df["Net"]>0).sum())/ (len(df)))*100, '%.',
-         df["Entry_Fee"].value_counts())
+         (((df["Net"]>0).sum())/ (len(df)))*100, '%.')
+pie_chart = px.pie(df, values=df['Entry_Fee'].value_counts(), names= df['Entry_Fee'].unique())
+st.write('To justify my claim of playing at relatively low stakes, here is the pie chart presenting all the entry fees and percentages at which I have played them at.',
+         pie_chart)
 
 
-st.write('##### To observe my levels of success of each sport, we present the bar graph of the net values every day for each specific sport.')
+
+
+st.write("##### To observe my levels of success of each sport, we present a bar graph of all the net values per day for each specific sport along with the sport's scatterplot")
 sports =df['Sport'].unique()
 selected_sport=st.selectbox('Select sport', sports, 4)
-sport_chart =alt.Chart(df[df['Sport']==selected_sport]).mark_bar().encode(
+           
+sport_chart1 =alt.Chart(df[df['Sport']==selected_sport]).mark_bar().encode(
     x = "Date",
     y = "Net",
-    color = "Place",
+    color = "Sport",
     tooltip= ['Date','Net']
 ).interactive()
+sport_chart = px.scatter(df[df['Sport']==selected_sport], x="Entry_Fee", y="Net", color="Sport", marginal_y="violin",
+           marginal_x="box")
+sport_chart1
 sport_chart
 
 
@@ -87,20 +97,23 @@ pick_date= st.date_input('',
 st.write("Date",pick_date)
 df[df['Date']==pick_date]
 
+
+
 st.write('### Linear Regression')
 
 
 reg_org= alt.Chart(df).mark_circle(size=60).encode(
     x='Place%',
     y='Winnings_Non_Ticket',
-    color='Sport',
-    tooltip=['Sport', 'Entry_Fee', 'Winnings_Non_Ticket', 'Place', 'Contest_Entries', ]
+    color = alt.Color('Sport',scale=alt.Scale(scheme="rainbow")),
+    tooltip=['Sport', 'Entry_Fee', 'Winnings_Non_Ticket', 'Place%' ]
 ).properties(
     width = 600,
     height = 500,
-).interactive()
-reg_org
+)
 
+
+st.write(reg_org + reg_org.transform_regression('Place%', 'Winnings_Non_Ticket').mark_line())
 
 reg = LinearRegression()
 
@@ -110,43 +123,79 @@ y= df['Winnings_Non_Ticket'].values.reshape(-1,1)
 linear_model.LinearRegression().fit(X,y)
 
 reg.fit(X,y)
-reg.coef_
-reg.intercept_
+st.write('The common themes with most of my scatterplots are that there exist two outliers and since my winning percentage is 24%, most of data is crowded towards the x-axis.'
+         'With that said the regression line from my data projects a coeficient of',
+         reg.coef_,
+         "for the 'place%' variable with an intercept of",
+         reg.intercept_,
+         'to project winnings based on my placement relative to the field.')
+st.latex(r'''
+         \widehat{Winnings} = -4.0835(Place Percentage)_i + 2.8687  
+         ''')
 
-reg_no_last= alt.Chart(df[df['Place%']<1]).mark_circle(size=60).encode(
+st.write('If I were to remove the two outlier points we would observe that the projected winnings would decresse but not by much since most of the data is already clustered towards the bottom.',
+         'The regression line for this case would also have a flatter decline with the absense of the outliers')
+st.latex(r'''
+         \widehat{Winnings} = -1.6041(Place Percentage)_i + 1.1782
+         ''')
+
+
+df_no_outliers= df[df['Winnings_Non_Ticket'] != 1000]
+reg_alt= alt.Chart(df_no_outliers).mark_circle(size=60).encode(
     x='Place%',
     y='Winnings_Non_Ticket',
-    color='Sport',
-    tooltip=['Sport', 'Entry_Fee', 'Winnings_Non_Ticket', 'Place', 'Contest_Entries', ]
-).properties(
-    width = 600,
-    height = 500,
-).interactive()
-reg_no_last
+    color = alt.Color('Sport',scale=alt.Scale(scheme="rainbow")),
+    tooltip=['Sport', 'Entry_Fee', 'Winnings_Non_Ticket', 'Place%' ]
+)
+st.write(reg_alt + reg_alt.transform_regression('Place%', 'Winnings_Non_Ticket').mark_line())
 
 reg = LinearRegression()
-df_reg= df[df['Place%']<1]
 
-X= df_reg['Place%'].values.reshape(-1,1)
-y= df_reg['Winnings_Non_Ticket'].values.reshape(-1,1)
+X= df_no_outliers['Place%'].values.reshape(-1,1)
+y= df_no_outliers['Winnings_Non_Ticket'].values.reshape(-1,1)
 
 linear_model.LinearRegression().fit(X,y)
 
 reg.fit(X,y)
-reg.coef_
-reg.intercept_
+# reg.coef_
+# reg.intercept_
 
 
+st.write('Next is a the result of an attempt to use a nueral network to fit ny data. The network consists of 3 layers and it does not look to be overfitting my dataset.'
+         'A reason I believe my data is difficult to fit is because there is no real pattern to what makes my bets win or not since they are practically an attempt on predicting the future.')
+numeric_cols = [c for c in df.columns if is_numeric_dtype(df[c])]
 
+scaler = StandardScaler()
+scaler.fit(df[numeric_cols])
 
+df[numeric_cols] = scaler.transform(df[numeric_cols])
+X_train = df[numeric_cols]
+y_train = df["Winnings_Non_Ticket"]
+model = keras.Sequential(
+    [
+        keras.layers.InputLayer(input_shape = (6,)),
+        #keras.layers.Flatten(),
+        keras.layers.Dense(16, activation="sigmoid"),
+        keras.layers.Dense(16, activation="sigmoid"),
+        keras.layers.Dense(1,activation="sigmoid")
+    ]
+    )
 
-reg_alt= alt.Chart(df[df['Winnings_Non_Ticket']!=1000]).mark_circle(size=60).encode(
-    x='Place%',
-    y='Winnings_Non_Ticket',
-    color='Sport',
-    tooltip=['Sport', 'Entry_Fee', 'Winnings_Non_Ticket', 'Place', 'Contest_Entries', ]
+model.compile(
+    loss="binary_crossentropy", 
+    optimizer=keras.optimizers.SGD(learning_rate=0.01),
+    metrics=["accuracy"],
 )
-reg_alt
+
+history = model.fit(X_train,y_train,epochs=100, validation_split = 0.2)
+
+fig, ax = plt.subplots()
+ax.plot(history.history['loss'])
+ax.plot(history.history['val_loss'])
+ax.set_ylabel('loss')
+ax.set_xlabel('epoch')
+ax.legend(['train', 'validation'], loc='upper right')
+fig
 
 
 
@@ -154,8 +203,7 @@ reg_alt
 
 
 
-
-
-
+st.write('I have implemented new types of charts throughout the project with the plotly library. Thank you for your time. The link for my Github repository is below')
+st.write('https://github.com/jimmyk2/Final_Project',unsafe_allow_html=True)
 
 
