3c4213be751fa77f35ebcf1dcda1f07e392dd574
jimmyk2 <91558577+jimmyk2@users.noreply.github.com>
2021-12-07 18:42:22 -0800

    Update Final_Project.py

--- Final_Project.py
+++ Final_Project.py
@@ -28,7 +28,7 @@ st.title("Math 10 Final Project")
 
 
 
-df = pd.read_csv("/Users/jimmy/Downloads/draftkings_contest_history_(11-23-21).csv", na_values=" ", )
+df = pd.read_csv("draftkings_contest_history_(11-23-21).csv", na_values=" ", )
 df["Date"]= df["Contest_Date_EST"].map(lambda x:x[:-5])
 df["Date"]= pd.to_datetime(df["Date"])
 df['Year']=df['Date'].dt.year
